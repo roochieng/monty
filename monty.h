fc62e2fa078f4d629df34d2043dc95acdffa6058
roochieng <49204011+roochieng@users.noreply.github.com>
2023-05-24 18:25:48 +0000

    Implement the pint opcode.

--- monty.h
+++ monty.h
@@ -12,10 +12,11 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -26,8 +27,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 #endif
